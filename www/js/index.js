6331c1119022c9e95fe0057be092044b451b7666
michele <micheledagruma92@hotmail.it>
2017-03-21 10:27:12 +0100

    implementazione form contatti col db

--- www/js/index.js
+++ www/js/index.js
@@ -6,4 +6,25 @@ $(function(){
   $("div[data-role='page']").on("swipeleft", function(event) {
     $(this).find("div[data-role='panel']").panel("close");
   });
+  //CHIAMATA CONTATTI
+  $("#invia").click(function(){
+    var object = {
+       nome : $("#nome").val(),
+       email : $("#email").val(),
+       oggetto : $("#oggetto").val(),
+       testo : $("#testo").val()
+    };
+
+      $.ajax({
+      url: 'https://francavilla-62b8e.firebaseio.com/contatti.json',
+      type: "POST",
+      dataType: 'json',
+      data: JSON.stringify(object)
+      }).done(function (data){
+        alert("Email inviata");
+      })
+      .fail(function(){
+        alert("Errore!");
+      });
+    });
 });
