3d6f317426d375683e98be36b13c5333e11a7062
michele <micheledagruma92@hotmail.it>
2017-03-24 18:51:23 +0100

    aggiunta mappa

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,15 @@
-//swipe home
+/* ---------- SWIPE PANEL ----------*/
 $(function(){
   $("div[data-role='page']").on("swiperight", function(event) {
     $(this).find("div[data-role='panel']").panel("open");
   });
   $("div[data-role='page']").on("swipeleft", function(event) {
     $(this).find("div[data-role='panel']").panel("close");
-  });
+  });/* ---------- FORM CONTATTI DINAMICO ----------*/
 
-  //CHIAMATA CONTATTI
+  /* ---------- SWIPE PANEL ----------*/
+
+  /* ---------- FORM CONTATTI DINAMICO ----------*/
   //#invia = id del bottone
   $("#invia").click(function(){
     var object = {
@@ -27,21 +29,19 @@ $(function(){
       .fail(function(){ // se c'è stato un problema
         alert("Errore!");
       });
-    });
+    });/* ---------- FORM CONTATTI DINAMICO ----------*/
 
-
-    //chiese
+    /* ---------- PAGINA CHIESE DINAMICA ----------*/
     $("#chiese").on('pagecreate', function() {
       $("#main-chiese").empty();
       $.ajax("https://francavilla-62b8e.firebaseio.com/luoghi/chiese.json")
       .done(function(chiese){
-        console.log(chiese);
         $.map(chiese, function(riga, indice) {
           var htmlPagina = "<div class='w3-container'>";
               htmlPagina += "<h1>"+riga.nome+"</h1>";
-              htmlPagina += "<h1>"+riga.immagine+"</h1>";
+              htmlPagina += "<img src="+riga.immagine+">";
               htmlPagina += "<p>"+riga.descrizione+"</p>";
-              htmlPagina += "<div>"+riga.coordinate+"</div>";
+              htmlPagina += "<a href="+riga.coordinate+">mappa</a>";
               htmlPagina += "</div>";
               $('#main-chiese').append(htmlPagina);
         })
@@ -49,6 +49,67 @@ $(function(){
       .fail(function(){
         alert("Errore! Prova a ricaricare la pagina...");
       });
-    });
+    });/* ---------- PAGINA CHIESE DINAMICA ----------*/
+
+
+    /* ---------- PAGINA CULTURA DINAMICA ----------*/
+    $("#cultura").on('pagecreate', function() {
+      $("#main-cultura").empty();
+      $.ajax("https://francavilla-62b8e.firebaseio.com/luoghi/cultura.json")
+      .done(function(cultura){
+        $.map(cultura, function(riga, indice) {
+          var htmlPagina = "<div class='w3-container'>";
+              htmlPagina += "<h1>"+riga.nome+"</h1>";
+              htmlPagina += "<img src="+riga.immagine+">";
+              htmlPagina += "<p>"+riga.descrizione+"</p>";
+              htmlPagina += "<a href="+riga.coordinate+">mappa</a>";
+              htmlPagina += "</div>";
+              $('#main-cultura').append(htmlPagina);
+        })
+      })
+      .fail(function(){
+        alert("Errore! Prova a ricaricare la pagina...");
+      });
+    });/* ---------- PAGINA CULTURA DINAMICA ----------*/
+
+    /* ---------- PAGINA ENOGASTRONOMIA DINAMICA ----------*/
+    $("#enogastronomia").on('pagecreate', function() {
+      $("#main-enogastronomia").empty();
+      $.ajax("https://francavilla-62b8e.firebaseio.com/luoghi/enogastronomia.json")
+      .done(function(enogastronomia){
+        $.map(enogastronomia, function(riga, indice) {
+          var htmlPagina = "<div class='w3-container'>";
+              htmlPagina += "<h1>"+riga.nome+"</h1>";
+              htmlPagina += "<img src="+riga.immagine+">";
+              htmlPagina += "<p>"+riga.indirizzo+"</p>";
+              htmlPagina += "<a href="+riga.coordinate+">mappa</a>";
+              htmlPagina += "</div>";
+              $('#main-enogastronomia').append(htmlPagina);
+        })
+      })
+      .fail(function(){
+        alert("Errore! Prova a ricaricare la pagina...");
+      });
+    });/* ---------- PAGINA ENOGASTRONOMIA DINAMICA ----------*/
+
+    /* ---------- PAGINA MONUMENTI DINAMICA ----------*/
+    $("#monumenti").on('pagecreate', function() {
+      $("#main-monumenti").empty();
+      $.ajax("https://francavilla-62b8e.firebaseio.com/luoghi/monumenti.json")
+      .done(function(monumenti){
+        $.map(monumenti, function(riga, indice) {
+          var htmlPagina = "<div class='w3-container'>";
+              htmlPagina += "<h1>"+riga.nome+"</h1>";
+              htmlPagina += "<h1>"+riga.immagine+"</h1>";
+              htmlPagina += "<p>"+riga.descrizione+"</p>";
+              htmlPagina += "<div>"+riga.coordinate+"</div>";
+              htmlPagina += "</div>";
+              $('#main-monumenti').append(htmlPagina);
+        })
+      })
+      .fail(function(){
+        alert("Errore! Prova a ricaricare la pagina...");
+      });
+    });/* ---------- PAGINA MONUMENTI DINAMICA ----------*/
 
 });
